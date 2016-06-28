e5d01087017ac7d9eeb64340bdcc7b38649c3151
Chris Grimes <cjg1981@live.com>
2016-06-27 20:03:33 -0500

    Don't use ES6 in here keep it light

--- src/curry.js
+++ src/curry.js
@@ -1,18 +1,17 @@
 'use strict';
 
 module.exports = function curry( fn ) {
-	let getArgs, arity;
+	var arity;
 
 	if ( !fn || typeof fn !== 'function' ) {
 		throw 'curry( fn ) - fn must be a function';
 	}
 
-	getArgs = ( args ) => [].slice.call( args, 0 );
 	arity = fn.length;
 
 	return function curried() {
-		let args;
-		args = getArgs( arguments );
+		var args;
+		args = [].slice.call( arguments, 0 );
 
 		if ( args.length >= arity ) {
 			return fn.apply( null, args );
@@ -20,7 +19,7 @@ module.exports = function curry( fn ) {
 		else {
 			return function partial() {
 				let args2;
-				args2 = getArgs( arguments );
+				args2 = [].slice.call( arguments, 0 );
 				return curried.apply( null, args.concat( args2 ) ); 
 			}
 		}
